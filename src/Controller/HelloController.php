3b1c1512641cf3c5c63b6f202dd91988c80ef76f
Mehdi <mehdi693@live.fr>
2021-05-31 14:10:00 +0200

    découverte Abstract Controller

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,30 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
 
-class HelloController
+class HelloController extends AbstractController
 {
-
     /**
      * @Route("/hello/{prenom?World}", name="hello")
      */
-    public function hello($prenom = "World", Environment $twig)
+    public function hello($prenom = "World")
     {
-        $html = $twig->render('hello.html.twig',[
-            'prenom' => $prenom,
-            'formateur1' => [
-                'prenom' => 'Mehdi',
-                'nom' => 'Banderas'
-            ],
-            'formateur2' => [
-                'prenom' => 'Zoé',
-                'nom' => 'Nathan'
-            ]
+        // $html = $this->twig->render('hello.html.twig',[
+        //     'prenom' => $prenom,
+        // ]);
+        // return new Response($html);
+        return $this->render('hello.html.twig', [
+            'prenom' => $prenom
         ]);
-        return new Response($html);
+    }
+
+    /**
+     * @Route("/example", name="example")
+     */
+    public function example() 
+    {
+        return $this->render('example.html.twig', [
+            'age' => 33
+            ]);
     }
 }
