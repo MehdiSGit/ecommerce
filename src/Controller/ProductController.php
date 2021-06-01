c4c8f1fe123a5a9559cef5e0c4c001efe0f1dd4b
Mehdi <mehdi693@live.fr>
2021-06-01 12:50:05 +0200

    createNotfoundExeption

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/{slug}", name="product_category")
+     */
+    public function category($slug, CategoryRepository $categoryRepository): Response
+    {   
+        $category = $categoryRepository->findOneBy([
+            'slug' => $slug
+        ]);
+            if (!$category) {
+                throw new NotFoundHttpException("La catégorie n'existe pas");
+            }
+        
+        return $this->render('product/category.html.twig', [
+            'slug' => $slug,
+        ]);
+    }
+}
