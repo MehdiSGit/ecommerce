0592b3891f9e7794f8e16ad54c5922b86b0e0853
Mehdi <mehdi693@live.fr>
2021-06-01 08:38:04 +0200

    entityManager

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,8 @@
 
 namespace App\Controller;
 
-use App\Repository\ProductRepository;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,7 +11,16 @@ class HomeController extends AbstractController {
     /**
      * @Route("/", name="homepage")
      */
-    public function homepage() {
+    public function homepage(EntityManagerInterface $em) {
+
+        $productRepository = $em->getRepository(Product::class);
+        $product = $productRepository->find(3);
+
+        $product->setPrice(2500);
+        $em->flush($product);
+
+        dd($product);
+
 
         return $this->render('home.html.twig', []);
 
