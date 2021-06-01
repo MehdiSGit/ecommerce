fade468ee1516b099d6de37d1b3c412ca1b002eb
Mehdi <mehdi693@live.fr>
2021-06-01 09:23:10 +0200

    require string (slug)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,19 +6,28 @@ use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AppFixtures extends Fixture
 {
+    protected $slugger;
+
+    public function __construct(SluggerInterface $slugger){
+        $this->slugger = $slugger;
+    }
+
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr_FR');
+        $faker->addProvider(new \Liior\Faker\Prices($faker));
+        $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
 
 
         for($p = 0; $p < 100; $p++) {
             $product = new Product();
-            $product->setName($faker->sentence())
-                    ->setPrice(mt_rand(100,200))
-                    ->setSlug($faker->slug());
+            $product->setName($faker->productName())
+                    ->setPrice($faker->price(4000, 20000))
+                    ->setSlug($this->slugger->slug($product->getName()));
 
             $manager->persist($product);
         }
