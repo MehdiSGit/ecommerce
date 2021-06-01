c82709d9ea4469b1ee208dd9ab16a6ba2573d0f8
Mehdi <mehdi693@live.fr>
2021-06-01 17:41:00 +0200

    fil d'ariane

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -25,6 +26,22 @@ class ProductController extends AbstractController
         return $this->render('product/category.html.twig', [
             'slug' => $slug,
             'category' => $category
+        ]);
+    }
+
+    /**
+     * @Route("/{category_slug}/{slug}", name="product_show")
+     */
+    public function show($slug, ProductRepository $productRepository) 
+    {
+        $product = $productRepository->findOneBy([
+            'slug' => $slug,
+        ]);
+        if(!$product){
+            throw $this->createNotFoundException("Le produit demandé n'existe pas");
+        }
+        return $this->render('product/show.html.twig',[
+            'product' => $product
         ]);
     }
 }
