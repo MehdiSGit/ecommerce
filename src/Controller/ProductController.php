efb75a4d87db29d94b9f7f4b8cbb5cdebe92c22e
Mehdi <mehdi693@live.fr>
2021-06-02 11:36:50 +0200

    form/producType

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,24 +2,18 @@
 
 namespace App\Controller;
 
-use App\Entity\Category;
+
 use App\Entity\Product;
+use App\Form\ProductType;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
-use Symfony\Component\Form\Extension\Core\Type\MoneyType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ProductController extends AbstractController
@@ -62,44 +56,19 @@ class ProductController extends AbstractController
     /**
      * @Route("/admin/product/create", name="product_create")
      */
-    public function create(FormFactoryInterface $factory, CategoryRepository $categoryRepository, Request $request, SluggerInterface $slugger) 
+    public function create(FormFactoryInterface $factory, CategoryRepository $categoryRepository, Request $request, SluggerInterface $slugger, EntityManagerInterface $em) 
     {   
-        $builder = $factory->createBuilder(FormType::class, null, [
-            'data_class' => Product::class
-        ]);
+        $form = $this->createForm(ProductType::class);
 
-        $builder->add('name',TextType::class, [
-            'label' => 'Nom du produit',
-            'attr' => ['placeholder' => 'Tapez le nom du produit']
-        ])
-                ->add('shortDescription', TextareaType::class, [
-                    'label' => 'Description courte',
-                    'attr' => ['placeholder' => 'Tapez une description']
-                ])
-                ->add('price', MoneyType::class, [
-                    'label' => 'Prix du produit',
-                    'attr' => ['placeholder' => 'Tapez le prix du produit']
-                ])
-                ->add('mainPicture', UrlType::class, [
-                    'label' => 'Image du produit',
-                    'attr' => ['placeholder' => 'Tapez une URL d\'image']
-                ])
-                ->add('category', EntityType::class, [
-                    'label' => 'Catégorie',
-                    'placeholder' => '--Choisir une catégorie',
-                    'class' => Category::class,
-                    'choice_label' => function(Category $category) {
-                        return strtoupper($category->getName());
-                    }
-                ]);
-        
-        $form = $builder->getForm();
         $form->handleRequest($request);
-        $data = $form->getData();
+        
         if($form->isSubmitted())
         {
             $product = $form->getData();
             $product->setSlug(strtolower($slugger->slug($product->getName())));
+
+            $em->persist($product);
+            $em->flush();
 
             // $product = new Product;
             // $product->setName($data['name'])
