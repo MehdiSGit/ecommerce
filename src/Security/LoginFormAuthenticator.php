3922bdaf3afc8b3c2b4e9020aa585579bd1a592d
Mehdi <mehdi693@live.fr>
2021-06-07 09:07:14 +0200

    path login/logout navbar

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -66,7 +66,7 @@ class LoginFormAuthenticator extends AbstractGuardAuthenticator
 
     public function start(Request $request, AuthenticationException $authException = null)
     {
-        // todo
+        return new RedirectResponse('/login');
     }
 
     public function supportsRememberMe()
