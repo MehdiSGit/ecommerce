7e0c84e300b2372c3ab77a3407b681dcc3fbfb31
Mehdi <mehdi693@live.fr>
2021-06-09 19:15:52 +0200

    page commandes + fixture setpurchedAt

---
+++ src/Controller/Purchase/PurchasesListController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller\Purchase;
+
+use App\Entity\User;
+use Twig\Environment;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+class PurchasesListController extends AbstractController
+{   
+    protected $security;
+    protected $routeur;
+    protected $twig;
+
+    public function __construct(Security $security, RouterInterface $routeur, Environment $twig)
+    {
+        $this->security = $security;
+        $this->routeur = $routeur;
+        $this->twig = $twig;
+    }
+
+    /**
+     * @Route("/purchases", name="pruchase_index")
+     */
+    public function index()
+    {
+        // 1. Nous devons nous assurer que la personne est connectée (sinon redirection vers la page d'accueil) -> Security
+        /** @var User */
+        $user = $this->security->getUser();
+
+        if(!$user)
+        {
+            throw new AccessDeniedException("Vous devez être connecté pour accéder à vos commandes");
+            // Générer une URL en fonction d'un nom d'une route ->UrlGeneratorInfterface
+        }
+
+        // 2. Nous voulons savoir qui est connécté ->securiy
+        // 3. Nous voulons passer l'utilisateur connecté à twig afin d'afficher ses commandes ->Environnement de twig / response
+        $html = $this->twig->render('purchase/index.html.twig', [
+            'purchases' => $user->getPurchases()
+        ]);
+        return new Response($html);
+    }
+}
+
