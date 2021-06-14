32976a7babdba18a5f2924393b251e818aea6ccc
Mehdi <mehdi693@live.fr>
2021-06-14 10:47:16 +0200

    life cycle callbacks

--- src/Purchase/PurchasePersister.php
+++ src/Purchase/PurchasePersister.php
@@ -24,9 +24,7 @@ class PurchasePersister
 
     public function storePurchase(Purchase $purchase)
     {   
-        $purchase->setUser($this->security->getUser())
-                ->setPurchasedAt(new DateTime())
-                ->setTotal($this->cartService->getTotal());
+        $purchase->setUser($this->security->getUser());
 
         $this->em->persist($purchase);
 
