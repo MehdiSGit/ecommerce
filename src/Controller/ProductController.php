ff93faf389c7fc519ee227bb188537d996616282
Mehdi <mehdi693@live.fr>
2021-06-17 10:29:33 +0200

    mailtrap

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,11 +4,13 @@ namespace App\Controller;
 
 
 use App\Entity\Product;
+use App\Event\ProductViewEvent;
 use App\Form\ProductType;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -40,15 +42,19 @@ class ProductController extends AbstractController
     /**
      * @Route("/{category_slug}/{slug}", name="product_show", priority= -1)
      */
-    public function show($slug, ProductRepository $productRepository) 
+    public function show($slug, ProductRepository $productRepository, EventDispatcherInterface $dispatcher) 
     {   
         
         $product = $productRepository->findOneBy([
             'slug' => $slug,
         ]);
+        
         if(!$product){
             throw $this->createNotFoundException("Le produit demandé n'existe pas");
         }
+
+        $dispatcher->dispatch(new ProductViewEvent($product), 'product.view');
+
         return $this->render('product/show.html.twig',[
             'product' => $product
         ]);
---
+++ src/EventDispatcher/ProductViewEmailSubscriber.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\EventDispatcher;
+
+use App\Event\ProductViewEvent;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
+
+class ProductViewEmailSubscriber implements EventSubscriberInterface
+{      
+    protected $logger;
+    protected $mailer;
+
+    public function __construct(LoggerInterface $logger, MailerInterface $mailer)
+    {
+        $this->logger = $logger;
+        $this->mailer = $mailer;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'product.view' => 'sendEmail'
+        ];
+    }
+
+    public function sendEmail(ProductViewEvent $productViewEvent)
+    {
+        $email = new Email();
+        $email->from(new Address("contact@mail.com", "Infos de la boutique"))
+            ->to("admin@mail.com")
+            ->text("un visiteur est entrain de voir la page du produit n°" . $productViewEvent->getProduct()->getId())
+            ->subject("Visite du produit n°" . $productViewEvent->getProduct()->getId());
+
+        // $this->mailer->send($email);
+
+        $this->logger->info("Email envoyé à l'admin pour le produit " . $productViewEvent->getProduct()->getId());
+    }
+}
